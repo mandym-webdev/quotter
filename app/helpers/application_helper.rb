f9968b9bebb52566c315b91c88926c855ef316fa
Mandy Moore <mandy@mandym.com>
2017-07-15 11:42:07 -0500

    added bootstrap classes to forms for styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def has_error?( model_class, attribute )
+    model_class.errors[attribute].size > 0
+  end
+
+  def error_for( model_class, attribute, err_class )
+    return err_class if has_error?
+    nil
+  end
 end
